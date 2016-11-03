9203162aee9f257ae1ee62963a60733db6464fee
amirmasoud <amirmasood33@gmail.com>
2016-11-03 18:49:09 +0330

    Add online/inapprove/available/onway scopes

--- app/Driver.php
+++ app/Driver.php
@@ -24,6 +24,74 @@ class Driver extends Model
     ];
 
     /**
+     * Scope a query to only include offline drivers.
+     * online    0
+     * available 0
+     * approve   1
+     * 
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeOffline($query)
+    {
+        return $query->where('online', 0)
+                     ->where('available', 0)
+                     ->where('approve', 1);
+    }
+
+    /**
+     * Scope a query to only include inapprove drivers.
+     * online    0
+     * available 0
+     * approve   0
+     * 
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeInapprove($query)
+    {
+        return $query->where('online', 0)
+                     ->where('available', 0)
+                     ->where('approve', 0);
+    }
+
+    /**
+     * Scope a query to only include onWay drivers.
+     * online    1
+     * available 0
+     * approve   1
+     * 
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeOnWay($query)
+    {
+        return $query->where('online', 1)
+                     ->where('available', 0)
+                     ->where('approve', 1);
+    }
+
+    /**
+     * Scope a query to only include available drivers.
+     * online    1
+     * available 1
+     * approve   1
+     * 
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeAvailable($query)
+    {
+        return $query->where('online', 1)
+                     ->where('available', 1)
+                     ->where('approve', 1);
+    }
+
+    /**
      * A driver can have one user.
      * 
      * @return hasOne
