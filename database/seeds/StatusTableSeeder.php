cb28ff97855d97a7d40f3287f6bb278ea872c7a4
amirmasoud <amirmasood33@gmail.com>
2016-11-03 18:37:39 +0330

    Add status table seeder

---
+++ database/seeds/StatusTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class StatusTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('status')->insert([
+        		[
+                    'name'  => 'request_taxi',
+                    'value' => 1,
+                ], [
+                    'name'  => 'client_found',
+                    'value' => 2,
+                ], [
+                    'name'  => 'no_response',
+                    'value' => 3,
+                ], [
+                    'name'  => 'reject_client_found',
+                    'value' => 4,
+                ], [
+                    'name'  => 'no_driver',
+                    'value' => 5,
+                ], [
+                    'name'  => 'trip_started',
+                    'value' => 6,
+                ], [
+                    'name'  => 'client_reject_started_trip',
+                    'value' => 7,
+                ], [
+                    'name'  => 'driver_reject_started_trip',
+                    'value' => 8,
+                ], [
+                    'name'  => 'trip_ended',
+                    'value' => 9,
+                ], [
+                    'name'  => 'cancel_request_taxi',
+                    'value' => 10,
+                ]
+            ]);
+    }
+}
