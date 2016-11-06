2d6788d9959b7911b96fcfeca924f667253c1f85
amirmasoud <amirmasood33@gmail.com>
2016-11-06 16:17:11 +0330

    Add social ID

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('social_id')->nullable();
             $table->string('picture')->default('no-profile.png');
             $table->rememberToken();
             $table->timestamps();
