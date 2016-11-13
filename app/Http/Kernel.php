aedf946da29f5c63096dacb48abe1170f76326f2
amirmasoud <amirmasood33@gmail.com>
2016-11-13 17:02:52 +0330

    Revert to default header

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -34,7 +34,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'accept',
             'throttle:60,1',
             'bindings',
         ],
@@ -58,6 +57,5 @@ class Kernel extends HttpKernel
         'throttle'   => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'bindings'   => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'accept' => \App\Http\Middleware\AcceptJson::class,
     ];
 }
