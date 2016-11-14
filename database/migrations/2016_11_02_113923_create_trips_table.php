6ad8e52be811f5008f8ac0023229ee4009d6b9f7
amirmasoud <amirmasood33@gmail.com>
2016-11-14 14:43:15 +0330

    Change order of trips and transactions migrations

--- database/migrations/2016_11_02_113923_create_trips_table.php
+++ database/migrations/2016_11_02_113923_create_trips_table.php
@@ -18,7 +18,8 @@ class CreateTripsTable extends Migration
             $table->unsignedInteger('driver_id');
             $table->foreign('driver_id')
                   ->references('id')->on('drivers')
-                  ->onDelete('cascade');
+                  ->onDelete('cascade')
+                  ->nullable();
             $table->unsignedInteger('client_id');
             $table->foreign('client_id')
                   ->references('id')->on('clients')
