3dbda3dd75199d4cabc8e9135c5d019470fc69fc
amirmasoud <amirmasood33@gmail.com>
2016-11-14 15:12:05 +0330

    Initial request taxi by client

---
+++ app/Http/Controllers/TripController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TripController extends Controller
+{
+	/**
+	 * Request taxi
+	 *
+	 * Request taxi by client.
+	 * @return json
+	 */
+    public function request()
+    {
+    	return;
+    }
+}
