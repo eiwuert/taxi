ca5de1b8fb06ef3f171a133e5b9f53c0ae058b0e
amirmasoud <amirmasood33@gmail.com>
2016-11-14 14:28:03 +0330

    Add trip fillable

--- app/Trip.php
+++ app/Trip.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Trip extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'start', 
+        'end',
+        'eta',
+        'etd',
+    ];
 }
