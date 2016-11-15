37159727611f9cbe6d57d48c72fedc4707f7ee0f
amirmasoud <amirmasood33@gmail.com>
2016-11-15 12:39:51 +0330

    Chack pending requst method

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use DB;
 use Auth;
+use App\Trip;
 use App\Status;
-use GoogleMaps;
 use App\Location;
 use Illuminate\Http\Request;
 use App\Http\Requests\TripRequest;
@@ -18,11 +18,15 @@ class TripController extends Controller
 	 * Request taxi by client.
 	 * @return json
 	 */
-    public function request(TripRequest $tripRequest)
+    public function requestTaxi(TripRequest $tripRequest)
     {
+    	if ($pending = $this->pendingRequestTaxi()) 
+    		return $pending;
+
     	$matrix = getDistanceMatrix($tripRequest->all());
     	$source = setLocation($tripRequest->s_lat, $tripRequest->s_long);
     	$destination = setLocation($tripRequest->d_lat, $tripRequest->d_long);
+
     	$result = DB::table('trips')->insert([
 						'client_id'   	  => Auth::user()->client()->first()->id,
 						'status_id'       => Status::where('name', 'request_taxi')->firstOrFail()->id,
@@ -51,5 +55,25 @@ class TripController extends Controller
     				'detail'=> 'There was some problem with inserting new trip to DB'
     			]);
     	}
+    }
+
+    /**
+     * Check pending request for current client that is requesting a new taxi.
+     * @return json
+     */
+    private function pendingRequestTaxi()
+    {
+    	$pending = Auth::user()->client()->first()->trips()
+    		   				  ->where('status_id', Status::where('name', 'request_taxi')->firstOrFail()->id);
+
+    	if ($pending->count()) {
+    		return fail([
+    				'title' => 'You have pending request',
+    				'detail'=> 'Please address your pending trip request at first',
+    				'trips' => $pending->get(),
+    			]);
+    	}
+
+    	return false;
     }
 }
