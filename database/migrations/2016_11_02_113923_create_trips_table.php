799a8cc3e0bcbc2df0f90bf8d76819c791b301c1
amirmasoud <amirmasood33@gmail.com>
2016-11-21 16:47:21 +0330

    Add driver start location

--- database/migrations/2016_11_02_113923_create_trips_table.php
+++ database/migrations/2016_11_02_113923_create_trips_table.php
@@ -42,6 +42,10 @@ class CreateTripsTable extends Migration
             $table->string('distance_text');
             $table->string('etd_value')->nullable();
             $table->string('etd_text')->nullable();
+            $table->unsignedInteger('driver_location');
+            $table->foreign('driver_location')
+                  ->references('id')->on('locations')
+                  ->onDelete('cascade');
             $table->string('driver_distance_value')->nullable();
             $table->string('driver_distance_text')->nullable();
         });
