8e85ccf81a04bdae1a2e829eb054efd741f47106
amirmasoud <amirmasood33@gmail.com>
2016-11-23 11:43:07 +0330

    Add make:logic command

---
+++ app/Console/Commands/MakeLogic.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use File;
+use Illuminate\Console\Command;
+
+class MakeLogic extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:logic {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new logic class';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        if (File::exists('app/Logics/' . $this->argument('name') . '.php')) {
+            $this->error($this->argument('name') . ' class already exists.');
+        } else {
+            File::put('app/Logics/' . $this->argument('name') . '.php', 
+"<?php
+
+namespace App\Logics;
+
+class {$this->argument('name')}
+{
+
+}
+");   
+            $this->info($this->argument('name') . ' class created.');
+        }
+    }
+}
