689c477b608cac318ac526632588b92d18986933
amirmasoud <amirmasood33@gmail.com>
2016-12-01 14:13:31 +0330

    Add car type seeder for initial car types

---
+++ database/seeds/CarTypeTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class CarTypeTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('car_types')->insert([[ 
+                    'name'       => 'luxury',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ], [ 
+                    'name'       => 'van',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ], [ 
+                    'name'       => 'sport',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ], [ 
+                    'name'       => 'sedans',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ], [ 
+                    'name'       => 'economy',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ], [ 
+                    'name'       => 'off-roader',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ], [ 
+                    'name'       => 'motorcycle',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ],
+            ]);
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(StatusTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(CarTypeTableSeeder::class);
     }
 }
