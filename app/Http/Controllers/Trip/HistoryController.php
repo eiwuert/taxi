f238454ade108d16cb7494b594a26c1e9198a8a3
amirmasoud <amirmasood33@gmail.com>
2016-12-20 12:38:54 +0330

    Add initial history controller

---
+++ app/Http/Controllers/Trip/HistoryController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Trip;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class HistoryController extends Controller
+{
+	/**
+	 * Get client trip history.
+	 * @return json
+	 */
+    public function client()
+    {
+        //
+    }
+
+    /**
+     * Get driver trip history
+     * @return json
+     */
+    public function driver()
+    {
+        //
+    }
+}
