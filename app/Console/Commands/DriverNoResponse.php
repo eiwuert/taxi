3633fad4b8b8986e554a1f9a7ee566bbd30c8549
amirmasoud <amirmasood33@gmail.com>
2017-01-14 12:24:30 +0330

    Fix fcm messages

--- app/Console/Commands/DriverNoResponse.php
+++ app/Console/Commands/DriverNoResponse.php
@@ -61,7 +61,7 @@ class driverNoResponse extends Command
                     'updated_at'             => Carbon::now(),
                 ]);
 
-            dispatch(new SendDriverNotification('4', 'no_reponse_going_offline', Driver::where('id', $trip->driver_id)->firstOrFail()->device_token));
+            dispatch(new SendDriverNotification('no_reponse_going_offline', '4', Driver::where('id', $trip->driver_id)->firstOrFail()->device_token));
 
             DB::table('drivers')
               ->where('id', $trip->driver_id)
@@ -83,7 +83,7 @@ class driverNoResponse extends Command
             if ($exclude['count'] < 1) {
                 $trip->requestTaxi($tripRequest, $exclude['result'], Client::find($prevTrip->client_id)->user->id);
             } else {
-                dispatch(new SendClientNotification('1', 'no_driver_found', Client::where('id', $prevTrip->client_id)->firstOrFail()->device_token));
+                dispatch(new SendClientNotification('no_driver_found', '1', Client::where('id', $prevTrip->client_id)->firstOrFail()->device_token));
             }
         }
     }
