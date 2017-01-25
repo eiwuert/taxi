3d3f563ba7e93366fd6a772b377738633e3de5cc
amirmasoud <amirmasood33@gmail.com>
2017-01-25 15:30:14 +0330

    Add logout path to route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 Route::get('/', 'HomeController@index');
+Route::post('logout', 'Auth\LoginController@logout');
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
