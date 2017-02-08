16c568cd4d107b86de260055b3e90509d6397230
amirmasoud <amirmasood33@gmail.com>
2017-02-08 17:23:20 +0330

    Fix setting highlight

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -24,18 +24,18 @@
         <li class="{{ (str_contains(Request::url(), route('clients.index'))) ? 'active' : '' }}"><a href="{{ route('clients.index') }}"><i class='ion-android-walk'></i><span> Clients</span></a></li>
         <li class="{{ (str_contains(Request::url(), route('trips.index'))) ? 'active' : '' }}"><a href="{{ route('trips.index') }}"><i class='ion-android-navigate'></i><span> Trips</span></a></li>
         <li class="{{ (str_contains(Request::url(), route('maps.index'))) ? 'active' : '' }}"><a href="{{ route('maps.index') }}"><i class='ion-map'></i><span> Maps</span></a></li>
-        <li class="treeview "{{ (str_contains(Request::url(), '/settings')) ? 'active' : '' }}"">
+        <li class="{{ (str_contains(Request::url(), 'admin/settings')) ? 'treeview active' : 'treeview' }}"">
           <a href="#"><i class="ion-gear-a"></i> <span>Settings</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="{{ route('settings.general') }}">General</a></li>
-            <li><a href="{{ route('settings.backup.index') }}">Backup</a></li>
-            <li><a href="{{ route('settings.logs.index') }}">Logs</a></li>
-            <li><a href="{{ route('settings.fcm.index') }}">FCM</a></li>
-            <li><a href="{{ route('settings.requests.index') }}">Requests</a></li>
+            <li class="{{ (str_contains(Request::url(), route('settings.general'))) ? 'active' : '' }}"><a href="{{ route('settings.general') }}">General</a></li>
+            <li class="{{ (str_contains(Request::url(), route('settings.backup.index'))) ? 'active' : '' }}"><a href="{{ route('settings.backup.index') }}">Backup</a></li>
+            <li class="{{ (str_contains(Request::url(), route('settings.logs.index'))) ? 'active' : '' }}"><a href="{{ route('settings.logs.index') }}">Logs</a></li>
+            <li class="{{ (str_contains(Request::url(), route('settings.fcm.index'))) ? 'active' : '' }}"><a href="{{ route('settings.fcm.index') }}">FCM</a></li>
+            <li class="{{ (str_contains(Request::url(), route('settings.requests.index'))) ? 'active' : '' }}"><a href="{{ route('settings.requests.index') }}">Requests</a></li>
           </ul>
         </li>
       </ul>
