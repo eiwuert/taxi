d83eabe99e3b754e3d83f97fdecf55bba31b99ab
amirmasoud <amirmasood33@gmail.com>
2017-02-19 10:53:05 +0330

    Fix #68

--- app/Http/Controllers/Admin/Setting/LogController.php
+++ app/Http/Controllers/Admin/Setting/LogController.php
@@ -21,6 +21,28 @@ class LogController extends Controller
         unset($logs[0]);
         $logs = array_reverse($logs);
         $logs = array_slice($logs, 0, 100);
+            // ENV
+            $logs = preg_replace("/^ [a-z]+/", '<b class="text-primary">${0}</b>', $logs);
+            // LEVEL
+            //
+            $logs = preg_replace_callback("/.[A-Z]+:/", 'static::color', $logs);
+            //preg_match("/[a-z]+.[A-Z]+: /", $log, $length);
         return view('admin.settings.logs', compact('logs'));
+    }
+
+    private static function color($match)
+    {
+        $text = trim(strtolower($match[0]));
+        $text = str_replace('.', '', $text);
+        $text = str_replace(':', '', $text);
+        if (str_contains($text, 'error') || str_contains($text, 'emergency') || 
+            str_contains($text, 'emergency') || str_contains($text, 'critical')) {
+            return '.<b class="text-danger">' . strtoupper($text) . '</b>: ';
+        } else if (str_contains($text, 'alert') || str_contains($text, 'warning') || 
+                   str_contains($text, 'alert')) {
+            return '.<b class="text-info">' . $text . '</b>: ';
+        } else {
+            return '.<b class="text-primary">' . $text . '</b>: ';
+        }
     }
 }
