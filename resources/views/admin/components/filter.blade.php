a82a2eb429980fc97aaae0d02dbdc62601494eee
amirmasoud <amirmasood33@gmail.com>
2017-03-01 17:34:20 +0330

    fix #40

--- resources/views/admin/components/filter.blade.php
+++ resources/views/admin/components/filter.blade.php
@@ -2,5 +2,14 @@
 $filters = Request::all();
 @endphp
 @foreach($filters as $filterKey => $filterValue)
-<span class="label label-primary">{{ $filterKey }}: {{ $filterValue }}</span>
+    @php 
+    if ($filterKey == 'date_range') $filterValue = str_replace('to', ' to ', $filterValue);
+    if ($filterKey == 'orderby') 
+        if($filterValue == 'asc') {
+            $filterValue = 'Ascending';
+        } else {
+            $filterValue = 'Descending';
+        }
+    @endphp
+    <span class="label label-primary"><b>{{ ucfirst(str_replace('_', ' ', $filterKey)) }}</b>: <i>{{ str_replace('_', ' ', $filterValue) }}</i></span>
 @endforeach
