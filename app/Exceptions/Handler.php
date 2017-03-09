e9eaf04673669460c2056addebfedf91d831f619
amirmasoud <amirmasood33@gmail.com>
2017-03-09 11:08:13 +0330

    Refactor exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -58,8 +58,8 @@ class Handler extends ExceptionHandler
     {
         if ($request->expectsJson()) {
             return response()->json(['error' => 'Unauthenticated.'], 401);
+        } else {
+            return redirect()->guest('admin/login');
         }
-
-        return redirect()->guest('admin/login');
     }
 }
