a5a87289f4d522c1265964f05bb2f6282706efe6
amirmasoud <amirmasood33@gmail.com>
2017-03-09 13:22:06 +0330

    Refactor

--- config/fcm.php
+++ config/fcm.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-	'client_server_key' => env('FCM_CLIENT_SERVER_KEY', 'Your FCM server key'),
-	'client_sender_id'  => env('FCM_CLIENT_SENDER_ID', 'Your sender id'),
-	'driver_server_key' => env('FCM_DRIVER_SERVER_KEY', 'Your FCM server key'),
-	'driver_sender_id'  => env('FCM_DRIVER_SENDER_ID', 'Your sender id'),
-	'send_url'		    => 'https://fcm.googleapis.com/fcm/send',
-	'group_url'		    => 'https://android.googleapis.com/gcm/notification',
-	'timeout'           => 40.0, // in second (default four weeks)
+    'client_server_key' => env('FCM_CLIENT_SERVER_KEY', 'Your FCM server key'),
+    'client_sender_id'  => env('FCM_CLIENT_SENDER_ID', 'Your sender id'),
+    'driver_server_key' => env('FCM_DRIVER_SERVER_KEY', 'Your FCM server key'),
+    'driver_sender_id'  => env('FCM_DRIVER_SENDER_ID', 'Your sender id'),
+    'send_url'          => 'https://fcm.googleapis.com/fcm/send',
+    'group_url'         => 'https://android.googleapis.com/gcm/notification',
+    'timeout'           => 40.0, // in second (default four weeks)
     'priority'          => 'high', // or normal (default is normal)
 ];
