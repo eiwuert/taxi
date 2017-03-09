1bceadefcbca23bf55b4932ab73aa091dbd6f98b
amirmasoud <amirmasood33@gmail.com>
2017-03-09 14:08:04 +0330

    Remove unused car request

--- app/Http/Controllers/Car/CarController.php
+++ app/Http/Controllers/Car/CarController.php
@@ -7,7 +7,6 @@ use App\Car;
 use App\User;
 use App\Http\Requests;
 use Illuminate\Http\Request;
-use App\Http\Requests\CarRequest;
 use App\Http\Controllers\Controller;
 
 class CarController extends Controller
