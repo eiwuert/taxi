0d4d44348947ffcbcb2cf2b2ccc4ec0db7363307
amirmasoud <amirmasood33@gmail.com>
2017-04-02 21:43:45 +0430

    Update GitLab CI

--- .gitlab-ci.sh
+++ .gitlab-ci.sh
@@ -7,13 +7,6 @@ set -xe
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
 apt-get install git -yqq
-sudo apt-get install -y postgresql postgresql-client libpq-dev
-sudo -u postgres psql -d template1
-CREATE USER postgres WITH PASSWORD 'root' CREATEDB;
-CREATE DATABASE saamtaxi OWNER postgres;
-\q
-psql -U postgres -h localhost -d saamtaxi -W
-
 
 # Install mysql driver
 # Here you can install any other extension that you need
