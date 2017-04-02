12636331956b48484d40aef29dcf387ee07ab56d
amirmasoud <amirmasood33@gmail.com>
2017-04-02 20:25:33 +0430

    Update gitlab ci

--- .gitlab-ci.sh
+++ .gitlab-ci.sh
@@ -7,6 +7,13 @@ set -xe
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
 apt-get install git -yqq
+sudo apt-get install -y postgresql postgresql-client libpq-dev
+sudo -u postgres psql -d template1
+CREATE USER root WITH PASSWORD 'root' CREATEDB;
+CREATE DATABASE saamtaxi OWNER root;
+\q
+psql -U runner -h localhost -d nice_marmot -W
+
 
 # Install mysql driver
 # Here you can install any other extension that you need
