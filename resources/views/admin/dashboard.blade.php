8d0a76cf057f8f58f94390e43537d7debf997bf2
amirmasoud <amirmasood33@gmail.com>
2017-04-05 12:41:40 +0430

    Make dashboard text transaltable

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,29 +1,29 @@
 @extends('admin.includes.layout')
 @section('title')
-dashboard
+@lang('admin/general.dashboard')
 @endsection
 @section('header')
-Dashboard
+@lang('admin/general.dashboard')
 @endsection
 
 @section('breadcrumb')
-<li class="active"><i class="fa fa-dashboard"></i> Dashboard</li>
+<li class="active"><i class="fa fa-dashboard"></i> @lang('admin/general.dashboard')</li>
 @endsection
 
 
 @section('content')
 <div class="row">
 	<div class="col-md-3">
-		<info-box text='Client' number='{{ $clients }}' color='aqua' icon='ion-android-walk'></info-box>
+		<info-box text='@lang('admin/general.client')' number='{{ $clients }}' color='aqua' icon='ion-android-walk'></info-box>
 	</div>
 	<div class="col-md-3">
-		<info-box text='Driver' number='{{ $drivers }}' color='aqua' icon='ion-model-s'></info-box>
+		<info-box text='@lang('admin/general.driver')' number='{{ $drivers }}' color='aqua' icon='ion-model-s'></info-box>
 	</div>
 	<div class="col-md-3">
-		<info-box text='trips' number='{{ $trips }}' color='aqua' icon='ion-android-navigate'></info-box>
+		<info-box text='@lang('admin/general.trips')' number='{{ $trips }}' color='aqua' icon='ion-android-navigate'></info-box>
 	</div>
 	<div class="col-md-3">
-		<info-box text='income' number='{{ $income }}' color='aqua' icon='ion-cash'></info-box>
+		<info-box text='@lang('admin/general.income')' number='{{ $income }}' color='aqua' icon='ion-cash'></info-box>
 	</div>
 </div>
 <div class="row">
@@ -31,7 +31,7 @@ Dashboard
 		@include('admin.components.googlemaps-markers', ['height' => '302px'])
 	</div>
 	<div class="col-md-6">
-		@include('admin.charts.flot-line', ['title' => 'Trips - ' . date('F', mktime(0, 0, 0, \Carbon\Carbon::now()->month, 10)) ])
+		@include('admin.charts.flot-line', ['title' => __('admin/general.trips') . ' - ' . date('F', mktime(0, 0, 0, \Carbon\Carbon::now()->month, 10)) ])
 	</div>
 </div>
 @endsection
