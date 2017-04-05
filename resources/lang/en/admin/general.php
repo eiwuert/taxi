aaf95b0c0f6518b50740537ec06300bbf15b4e57
amirmasoud <amirmasood33@gmail.com>
2017-04-05 12:41:55 +0430

    Add semi-auto transaltor

---
+++ resources/lang/en/admin/general.php
@@ -0,0 +1,18 @@
+<?php
+/*************************************************************************
+ Generated via "php artisan localization:missing" at 2017/04/05 07:35:46 
+*************************************************************************/
+
+return array (
+  //============================== New strings to translate ==============================//
+  // Defined in file /Applications/MAMP/htdocs/saamtaxi/resources/views/admin/dashboard.blade.php
+  'client' => 'Client',
+  // Defined in file /Applications/MAMP/htdocs/saamtaxi/resources/views/admin/dashboard.blade.php
+  'driver' => 'Driver',
+  // Defined in file /Applications/MAMP/htdocs/saamtaxi/resources/views/admin/dashboard.blade.php
+  'income' => 'Income',
+  // Defined in file /Applications/MAMP/htdocs/saamtaxi/resources/views/admin/dashboard.blade.php
+  'trips' => 'Trips',
+  //==================================== Translations ====================================//
+  'dashboard' => 'dashboard',
+);
