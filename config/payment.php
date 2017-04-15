c678d76e061ee7ce2db7867baaaf4b7c66630efe
amirmasoud <amirmasood33@gmail.com>
2017-04-15 15:31:25 +0430

    Update payment redirect back url

--- config/payment.php
+++ config/payment.php
@@ -3,7 +3,7 @@
  * IPG related data.
  */
 return [
-    'back' => env('APP_URL') . '/payment/charge',
+    'back' => env('APP_URL') . '/fa/payment/charge',
     'username' => '21339760',
     'password' => '21350400',
     'ipg' => 'https://fcp.shaparak.ir/ref-payment/jax/merchantAuth?wsdl',
