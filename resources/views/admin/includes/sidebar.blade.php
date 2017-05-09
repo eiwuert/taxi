b90b2a280d922c8a878c9adce5d3e29c2e239bea
amirmasoud <amirmasood33@gmail.com>
2017-05-09 18:08:37 +0430

    Remove client from agency admin

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -18,9 +18,12 @@
             <!-- Optionally, you can add icons to the links -->
             <li class="{{ (str_contains(Request::url(), route('dashboard'))) ? 'active' : '' }}"><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i><span>@lang('admin/general.Dashboard') </span></a></li>
             <li class="{{ (str_contains(Request::url(), route('drivers.index'))) ? 'active' : '' }}"><a href="{{ route('drivers.index') }}"><i class='ion-model-s'></i><span>@lang('admin/general.Drivers') </span></a></li>
+            @if (Auth::user()->web->superadmin())
             <li class="{{ (str_contains(Request::url(), route('clients.index'))) ? 'active' : '' }}"><a href="{{ route('clients.index') }}"><i class='ion-android-walk'></i><span> @lang('admin/general.Clients')</span></a></li>
+            @endif
             <li class="{{ (str_contains(Request::url(), route('trips.index'))) ? 'active' : '' }}"><a href="{{ route('trips.index') }}"><i class='ion-android-navigate'></i><span>  @lang('admin/general.Trips')</span></a></li>
             <li class="{{ (str_contains(Request::url(), route('maps.index'))) ? 'active' : '' }}"><a href="{{ route('maps.index') }}"><i class='ion-map'></i><span> @lang('admin/general.Maps')</span></a></li>
+            <li class="{{ (str_contains(Request::url(), route('agencies.index'))) ? 'active' : '' }}"><a href="{{ route('agencies.index') }}"><i class='ion-map'></i><span> @lang('admin/general.Agencies')</span></a></li>
             <li class="{{ (str_contains(Request::url(), 'admin/payments')) ? 'treeview active' : 'treeview' }}">
                 <a href="#"><i class="ion-card"></i> <span>@lang('admin/general.Payments')</span>
                 @include('admin.includes.angle')
