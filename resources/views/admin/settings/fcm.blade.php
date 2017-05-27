e5db2cdbc1e0bbf4ad21018de972de2de93cf1f7
Hossein <hossein.nasiry@yahoo.com>
2017-05-27 13:24:09 +0430

    add searing and filtering to show fcm logs

--- resources/views/admin/settings/fcm.blade.php
+++ resources/views/admin/settings/fcm.blade.php
@@ -6,9 +6,45 @@
 @endsection
 @section('content')
 @include('admin.charts.fcm')
+
+<div class="box box-solid">
+    <div class="box-header with-border">
+        <i class="fa fa-filter"></i>
+        <h3 class="box-title">@lang('admin/general.Filter')</h3>
+    </div>
+    <!-- /.box-header -->
+    <div class="box-body">
+    {!! Form::open(['action' => 'Admin\Setting\FcmController@filter', 'method' => 'get', 'class' => 'form-inline']) !!}
+    @include('components.bootstrap.select', ['name' => 'sortby',
+    'label' => __('admin/general.Sort by'),
+    'items' => \App\Fcm::$sortable])
+    @include('components.bootstrap.select', ['name' => 'orderby',
+    'label' => __('admin/general.Order by'),
+    'items' => ['asc' => __('admin/general.Ascending'), 'desc' => __('admin/general.Descending')]])
+    @include('components.bootstrap.select', ['name' => 'count',
+    'label' => __('admin/general.Count'),
+    'items' => [15 => 15, 30 => 30, 100 => 100, 'all' => 'All']])
+    {!! Form::close() !!}
+    <!-- /.box-body -->
+    </div>
+    <div class="box-footer">
+        @include('admin.components.filter')
+    </div>
+</div>
+
 <div class="box box-solid">
     <div class="box-header">
         <h3 class="box-title">@lang('admin/general.FCM Logs')</h3>
+        <div class="box-tools">
+            {!! Form::open(['action' => 'Admin\Setting\FcmController@search', 'method' => 'get']) !!}
+            <div class="input-group input-group-sm" style="width: 150px;">
+                <input type="text" name="q" class="form-control pull-right" placeholder="@lang('admin/general.Search')" value="{{ Request::get('q') }}">
+                <div class="input-group-btn">
+                    <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                </div>
+            </div>
+            {!! Form::close() !!}
+        </div>
     </div>
     <!-- /.box-header -->
     <div class="box-body table-responsive no-padding">
