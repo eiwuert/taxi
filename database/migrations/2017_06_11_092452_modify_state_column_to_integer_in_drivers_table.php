77c83ff2613243b0e48ddd44eb55a73f837384be
Hossein <hossein.nasiry@yahoo.com>
2017-06-11 09:49:40 +0430

    modify state column to integer type

---
+++ database/migrations/2017_06_11_092452_modify_state_column_to_integer_in_drivers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyStateColumnToIntegerInDriversTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('drivers', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('drivers', function (Blueprint $table) {
+            DB::statment('alter table '.$table->getTable().' alter column state type numeric(10,0) using state::numeric;');
+        });
+    }
+}
