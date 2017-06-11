9fa9328271ae09a292125bb3bb95c10550b1740a
amirmasoud <amirmasood33@gmail.com>
2017-06-11 18:19:22 +0430

    Compile assets

--- resources/views/admin/cars/edit.blade.php
+++ resources/views/admin/cars/edit.blade.php
@@ -18,3 +18,16 @@
   </div>
 </div>
 {!!  Form::close() !!}
+@push('js')
+<script src="{{ mix('js/admin/typeahead.js') }}"></script>
+<script>
+$('.typeahead').typeahead({
+  minLength: 3,
+  highlight: true
+},
+{
+  name: 'my-dataset',
+  source: ['mySource']
+});
+</script>
+@endpush
