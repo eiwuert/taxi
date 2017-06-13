1d387b331d46670880c055838c50394d420ac83c
amirmasoud <amirmasood33@gmail.com>
2017-06-13 17:04:36 +0430

    Fix phone number without adding extra col to sms table

--- app/Http/Controllers/Admin/Setting/SmsController.php
+++ app/Http/Controllers/Admin/Setting/SmsController.php
@@ -12,44 +12,12 @@ class SmsController extends Controller
 {
     /**
      * Show all codes.
+     * 
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        $codes = Sms::with('user')
-                    ->orderBy('id', 'desc')
-                    ->paginate((int) option('pagination', 15));
-        $codes = $this->mutateCodes($codes);
-        // $client = new SoapClient('http://ip.sms.ir/ws/SendReceive.asmx?wsdl');
-        // $Params= [
-        //     'userName'=>'09122641637',
-        //     'password'=>'!@#123QWEqwe',
-        //     'fromDate'=>Carbon::now()->yesterday()->endOfDay()->toIso8601String(),
-        //     'toDate'=>Carbon::now()->endOfDay()->toIso8601String()
-        // ];
-        // dd( $client->GetVipSentMessages($Params) );
+        $codes = Sms::orderBy('id', 'desc')->paginate(option('pagination', 15));
         return view('admin.settings.sms', compact('codes'));
-    }
-
-    /**
-     * Mutate codes before sending to view.
-     *
-     * @param  Array $codes
-     * @return \Illuminate\Http\Response
-     */
-    private function mutateCodes($codes)
-    {
-        foreach ($codes as $code) {
-            $user = call_user_func([$code->user, $code->user->role])->first();
-            $code->name = $code->user->name();
-            if (is_null($user)) {
-                $code->picture = asset('img/no-profile.png');
-                $code->phone = null;
-            } else {
-                $code->picture = $user->getPicture();
-                $code->phone = $code->user->phone();
-            }
-        }
-        return $codes;
     }
 }
--- resources/views/admin/settings/sms.blade.php
+++ resources/views/admin/settings/sms.blade.php
@@ -14,8 +14,6 @@
         <table class="table">
             <tbody>
                 <tr>
-                    <th></th>
-                    <th>@lang('admin/general.Name')</th>
                     <th>@lang('admin/general.Phone')</th>
                     <th>@lang('admin/general.Code')</th>
                     <th>@lang('admin/general.Attempts')</th>
@@ -23,12 +21,7 @@
                 </tr>
                 @foreach($codes as $code)
                 <tr>
-                    <td><img src="{{ $code->picture }}"
-                             alt="User profile picture"
-                             class="img-responsive img-circle"
-                             width="24"></td>
-                    <td>{!! $code->name or '<tag color="default">In progress</tag>' !!}</td>
-                    <td>{!! $code->phone or '<tag color="success">Registering</tag>' !!}</td>
+                    <td>{{ $code->user->phone }}</td>
                     <td>{{ $code->code }}</td>
                     <td>{{ $code->attempts }}</td>
                     <td>{{ $code->created_at->diffForHumans() }}</td>
