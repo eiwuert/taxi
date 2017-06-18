dbf845e20f3aa47049769071564d2333c6b0cf50
amirmasoud <amirmasood33@gmail.com>
2017-06-18 11:08:04 +0430

    Add favicon

--- resources/views/admin/includes/head.blade.php
+++ resources/views/admin/includes/head.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>{{ config('app.name') }} &bull; @yield('title', '')</title>
+  <title>@lang('admin/general.Flip') &bull; @yield('title', '')</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <script>
@@ -11,6 +11,7 @@
               'csrfToken' => csrf_token(),
           ]); ?>
   </script>
+  <link rel="shortcut icon" type="image/png" href="{{ asset('images/favicon.png') }}"/>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
   <link rel="stylesheet" href="{{ mix('css/admin/admin.css') }}">
